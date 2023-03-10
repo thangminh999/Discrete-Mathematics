b936306ec9e1b1e95d765862ad3bdf761f53bd42
@thangminh999 <hoangngocminhthang@gmail.com>
2023-03-11 00:33:11 +0700

    test

--- btnhom4/Program.cs
+++ btnhom4/Program.cs
@@ -13,6 +13,17 @@ namespace btnhom4
         {
             int[] nums = { 1, 2, 3, 4 };
             SinhNhiPhan(nums, "C:\\Users\\Thang\\Desktop\\bt-TRR\\output.txt");
+            /*Console.WriteLine("Nhap vao so phan tu cua mang nums (toi da 4 phan tu): ");
+            int n = int.Parse(Console.ReadLine());
+            n = Math.Min(n, 4);
+            int[] nums = new int[n];
+            Console.WriteLine("Nhap vao cac phan tu cua mang nums: ");
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+            SinhNhiPhan(nums, "C:\\Users\\Thang\\Desktop\\bt-TRR\\output.txt");*/
+
         }
         public static void SinhNhiPhan(int[] nums, string filename)
         {
